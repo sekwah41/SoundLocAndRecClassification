47157f08a88ea14ce2f8abccd6ce53b09b8d5206
Sekwah <sekwah41@gmail.com>
2020-04-21 23:16:58 +0100

    Splitting into files

--- src/tensorflow_test.py
+++ src/tensorflow_test.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
-print(tf.test.gpu_device_name())
-print(tf.test.is_gpu_available())
-print(tf.test.is_built_with_gpu_support())
+print("CPU's:", tf.config.list_physical_devices('CPU'))
+print("GPU's:", tf.config.list_physical_devices('GPU'))
+print("GPU Available:", tf.test.is_gpu_available())
+print("GPU Support:", tf.test.is_built_with_gpu_support())
+print("CUDA Support:", tf.test.is_built_with_cuda())
